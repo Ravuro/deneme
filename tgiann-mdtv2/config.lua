ab09b392cf01ce8cef893548ca21b03e512fdbd9
Ravuro <kadiruzunoff@gmail.com>
2025-07-16 00:49:51 +0300

    Delete tgiann-mdtv2 directory

--- tgiann-mdtv2/config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.Notify = 'qb' -- mythic (new) / esx
-Config.Webhook = 'WEBHOOKLARIGİRİNNNNN/1205658378039205888/H2cqPlaqpJa9vHeoDshiJ06UKKYHSgFDo7Dm7JMY65aVsq9CkflKqAzE1G4n_rqApmdH' -- Place your discord webhook here for using screenshot feature 
-Config.KeyMapping = 'delete' -- Control action for open the MDT
